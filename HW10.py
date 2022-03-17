3bd12d75ec3cad8ce647bef8454c4cb340c76230
Evgenii Bobarniov <kabanos1@yandex.ru>
2022-03-17 22:57:54 +0300

    Home work 10.

---
+++ HW10.py
@@ -0,0 +1,42 @@
+def get_number(question: str) -> float:
+    while True:
+        try:
+            return float(input(question))
+        except ValueError:
+            print("Ну-ка стринги не пиши мне тут!")
+
+
+def calculate():
+    operation = input('''
+Выбери что нужно делать:
++ сумма
+- вычитание
+* умножение
+/ деление
+''')
+    number_1 = get_number("Ввести число номер раз ")
+    number_2 = get_number("Ввести число номер два ")
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        try:
+            print(number_1 / number_2)
+        except Exception as e:
+            print(f"Не дели на ноль:{e}")
+    else:
+        raise InvalidExeption(f"Выбери из предложенных, БАКА!\nТы ввёл {operation}")
+
+
+class InvalidExeption(Exception):
+    pass
+
+
+calculate()
