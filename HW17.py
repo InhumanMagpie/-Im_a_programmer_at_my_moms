02bb0e1bca6b591997c2b665e911a9e8499e17f6
Evgenii Bobarniov <kabanos1@yandex.ru>
2022-04-11 18:06:02 +0300

    Homework 17 fixes with foreign key.

--- HW17.py
+++ HW17.py
@@ -1,8 +1,5 @@
 import json
-from models.user import User
-from models.event import Event
 from typing import Optional
-
 from flask import Flask, request, Response
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +9,48 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhos
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+events = db.Table("user",
+                  db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+                  db.Column("event_id", db.Integer, db.ForeignKey("event.id"), primary_key=True)
+                  )
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    __table_args__ = {"extend_existing": True}
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    city = db.Column(db.String(20), nullable=False, default="Unknown")
+    age = db.Column(db.Integer, nullable=True)
+    events = db.relationship("Event", secondary=events, lazy="subquery",
+                             backref=db.backref("users", lazy=True))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'age': self.age,
+            'email': self.email,
+            'city': self.city
+        }
+
+
+class Event(db.Model):
+    __tablename__ = "event"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    event_name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<Event %r>" % self.event_name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "event_name": self.event_name
+        }
 
 
 @app.route("/user/", methods=["POST"])
