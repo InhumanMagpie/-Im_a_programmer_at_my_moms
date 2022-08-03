bbff70ddc04785b7b77eb2bd087105924155901b
Evgenii Bobarniov <kabanos1@yandex.ru>
2022-08-03 18:33:24 +0300

    Team project

--- myproject/teamwork/admin.py
+++ myproject/teamwork/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import User, Team, Task
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # fieldsets = (
+    #     (None, {"fields": ("username", "password")}),
+    #     (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+    #     (
+    #         _("Permissions"),
+    #         {
+    #             "fields": (
+    #                 "is_active",
+    #                 "is_staff",
+    #                 "is_superuser",
+    #             ),
+    #         },
+    #     ),
+    # )
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
